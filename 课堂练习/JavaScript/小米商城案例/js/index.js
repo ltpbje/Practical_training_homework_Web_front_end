85156260d8fb29b951e8e935e7ce90b76114ac47
乐天居易 <19836788252@163.com>
2024-10-08 20:05:16 +0800

    今天的课堂练习

--- 课堂练习/JavaScript/小米商城案例/js/index.js
+++ 课堂练习/JavaScript/小米商城案例/js/index.js
@@ -18,3 +18,24 @@ function changePlaceholder() {
 };
 
 setInterval(changePlaceholder, 3000);
+
+// ---------------------search------------------------
+const nav_items = document.querySelectorAll('.nav-item li'); 
+const con_items = document.querySelectorAll('.con_item');
+const second_nav = document.querySelector(".second-nav");
+const header = document.querySelector('.header');
+for (let i = 0; i < nav_items.length; i++){
+    nav_items[i].addEventListener('mouseover', function () {
+        // console.log(i);
+        // second_nav.style.boxShadow  = '0 2px 2px #D6D6D6;'
+        second_nav.style.height = '212px'
+        for (let j = 0; j < con_items.length; j++){
+            con_items[j].style.display ='none'
+        }
+        con_items[i].style.display ='block'
+    })
+    header.addEventListener('mouseleave', function () {
+        second_nav.style.height = '0px'
+        // second_nav.style.boxShadow = 'none'
+    })
+}
