8d031a8fc20e4304ee2a01a4406ea0d1a1064c9a
乐天居易 <19836788252@163.com>
2024-10-28 15:49:00 +0800

    1

--- 课堂练习/移动端/飞机大战案例/js/Background.js
+++ 课堂练习/移动端/飞机大战案例/js/Background.js
@@ -4,7 +4,9 @@
 
 import assetsConfig from "./assetsConfig.js";
 
+// 定义一个背景类
 class Background {
+    // 构造函数，初始化背景的坐标、图片、宽度和高度
     constructor() {
         this.x = 0;
         this.y = 0;
@@ -12,6 +14,7 @@ class Background {
         this.width = this.img.width;
         this.height = this.img.height;
     }
+    // 绘制背景
     draw(ctx) {
         ctx.drawImage(this.img,
             this.x,
--- 课堂练习/移动端/飞机大战案例/js/index.js
+++ 课堂练习/移动端/飞机大战案例/js/index.js
@@ -9,6 +9,10 @@ const gameControl = {
         gameHeight: 768,
         ctx: null
     },
+    gameContainer: {
+        //游戏容器，专业存入在页面上需要使用的游戏对象
+        bg: null
+    },
     bindEvent() {
 
     },
@@ -20,7 +24,10 @@ const gameControl = {
         this.data.ctx = this.dom.game.getContext('2d');
         // 加载游戏资源
         await assetsConfig.loadAssets(); ///调用加载资源的方法
-
+        // 创建一个背景对象
+        this.gameContainer.bg = new Background();
+        // 在画布上绘制背景
+        this.gameContainer.bg.draw(this.data.ctx);
     }
 };
 
