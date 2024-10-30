cd3710d4f3d7a59876c133dda99b0347fc1dd6fe
乐天居易 <19836788252@163.com>
2024-10-30 21:39:43 +0800

    backups

--- 课堂练习/移动端/天气案例/js/index.js
+++ 课堂练习/移动端/天气案例/js/index.js
@@ -11,7 +11,7 @@ function touchMove(e) {
     // console.log(slide_box.style.transform);
     // console.log(1, slide_box.getBoundingClientRect().x);
     const finalX = slide_box_X + moveX;
-    console.log(slide_box_X + moveX);
+    // console.log(slide_box_X + moveX);
     // slide_box.getBoundingClientRect().x = 40;
     slide_box.style.transform = `translateX(${finalX}px)`;
     // console.log(moveX);
@@ -30,11 +30,24 @@ slide_box.addEventListener('touchstart', function (e) {
     slide_box_X = slide_box.getBoundingClientRect().x;
     // 记录刚触摸时候的位置
     startX = e.touches[0].clientX;
+
     // console.log(e.touches[0].clientX);
     slide_box.addEventListener('touchmove', touchMove);
     slide_box.addEventListener('touchend', function () {
-        // console.log(1);
+        console.log(slide_box.getBoundingClientRect().x);
+        if (slide_box.getBoundingClientRect().x > 0) {
+            // console.log(1);
 
+            slide_box.style.transform = `translateX(0px)`;
+        }
+        // console.log(-1 * (slide_box.clientWidth - document.querySelector('.future_item').clientWidth));
+        // 滑块向左移动的最大距离
+        const maxLeft = -1 * (slide_box.clientWidth - document.querySelector('.future_item').clientWidth);
+        if (slide_box.getBoundingClientRect().x < maxLeft) {
+            console.log(1);
+
+            slide_box.style.transform = `translateX(${maxLeft}px)`;
+        }
         slide_box.removeEventListener('touchmove', touchMove);
     });
 });
