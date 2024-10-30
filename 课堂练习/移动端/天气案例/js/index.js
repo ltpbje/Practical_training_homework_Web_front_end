d19a4323caa5d50b9db66351deaa697add7cec81
乐天居易 <19836788252@163.com>
2024-10-30 21:10:25 +0800

    backups

---
+++ 课堂练习/移动端/天气案例/js/index.js
@@ -0,0 +1,40 @@
+// 滑动盒子
+const slide_box = document.querySelector('.slide_box');
+let startX = 0;
+let slide_box_X;
+function touchMove(e) {
+
+    // console.log(e);
+    // 触摸时候的位置
+    const currentX = e.touches[0].clientX;
+    const moveX = currentX - startX;
+    // console.log(slide_box.style.transform);
+    // console.log(1, slide_box.getBoundingClientRect().x);
+    const finalX = slide_box_X + moveX;
+    console.log(slide_box_X + moveX);
+    // slide_box.getBoundingClientRect().x = 40;
+    slide_box.style.transform = `translateX(${finalX}px)`;
+    // console.log(moveX);
+
+    // console.log('x', moveX);
+
+    // slide_box.addEventListener('touchend', function () {
+    //     console.log(1);
+
+    //     slide_box.removeEventListener('touchmove', touchMove);
+    // });
+
+}
+
+slide_box.addEventListener('touchstart', function (e) {
+    slide_box_X = slide_box.getBoundingClientRect().x;
+    // 记录刚触摸时候的位置
+    startX = e.touches[0].clientX;
+    // console.log(e.touches[0].clientX);
+    slide_box.addEventListener('touchmove', touchMove);
+    slide_box.addEventListener('touchend', function () {
+        // console.log(1);
+
+        slide_box.removeEventListener('touchmove', touchMove);
+    });
+});
