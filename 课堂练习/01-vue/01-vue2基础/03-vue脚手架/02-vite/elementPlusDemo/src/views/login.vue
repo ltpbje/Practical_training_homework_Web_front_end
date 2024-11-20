4fae68b286068d4b5a6a64269061b04dfbac565c
ltpbje <19836788252@163.com>
2024-11-20 16:30:43 +0800

    backups

--- 课堂练习/01-vue/01-vue2基础/03-vue脚手架/02-vite/elementPlusDemo/src/views/login.vue
+++ 课堂练习/01-vue/01-vue2基础/03-vue脚手架/02-vite/elementPlusDemo/src/views/login.vue
@@ -1,15 +1,37 @@
 <script setup>
 import PageView from './PageView.vue';
+import { reactive } from 'vue';
+const formData = reactive({
+    zh: '',
+    password: ''
+});
+const rules = reactive({
+    zh: [
+        {
+            required: true,
+            message: '必须输入用户名',
+            trigger: 'blur'
+        }
+    ],
+    password: [
+        {
+            required: true,
+            message: '必须输入密码',
+            trigger: 'blur'
+        }
+    ]
+});
+
 </script>
 <template>
     <PageView class="flex-row j-c a-c">
         <div class="login">
-            <el-form label-width="60px">
-                <el-form-item label="用户名">
-                    <el-input />
+            <el-form :model="formData" label-width="80px" :rules="rules" status-icon>
+                <el-form-item label="用户名" prop="zh">
+                    <el-input v-model="formData.zh" />
                 </el-form-item>
-                <el-form-item label="密码">
-                    <el-input type="password" />
+                <el-form-item label="密码" prop="password">
+                    <el-input type="password" v-model="formData.password" />
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary">登录</el-button>
@@ -25,7 +47,7 @@ import PageView from './PageView.vue';
 
 <style lang="scss" scoped>
 .login {
-    width: 350px;
+    width: 380px;
     // display: flex;
 }
 </style>
