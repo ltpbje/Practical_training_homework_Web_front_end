40fe5a952592f9e6d2a8f8196b90d199ff3f866d
ltpbje <19836788252@163.com>
2024-11-20 18:12:22 +0800

    作业

---
+++ 作业/专业方向强化课程3 - 作业2-11-20/my-vue-app/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="box">
+    <div class="title">This is a Todolist</div>
+    <input type="text" class="inpEl">
+  </div>
+</template>
+
+<style scoped>
+.box {
+  width: 100vw;
+  min-height: 100px;
+  margin-top: 100px;
+  background-image: url(./assets/img/bg.jpg);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.box .title {
+  font-size: 40px;
+  font-weight: bold;
+}
+
+.inpEl {
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid #CFCFCF;
+}
+</style>
