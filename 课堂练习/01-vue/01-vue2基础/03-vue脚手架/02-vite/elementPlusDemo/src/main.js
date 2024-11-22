765698e1f362ea049d96fae63a802dce28a86b29
ltpbje <19836788252@163.com>
2024-11-22 08:23:59 +0800

    backups

--- 课堂练习/01-vue/01-vue2基础/03-vue脚手架/02-vite/elementPlusDemo/src/main.js
+++ 课堂练习/01-vue/01-vue2基础/03-vue脚手架/02-vite/elementPlusDemo/src/main.js
@@ -5,6 +5,19 @@ import router from './router';
 import { createPinia } from 'pinia';
 import piniaPersist from 'pinia-plugin-persist';
 import 'element-plus/dist/index.css';
+import { userLoginInfo } from './store/login';
+const store = userLoginInfo();
+router.beforeEach((to, from, next) => {
+    if (to.name === 'login') {
+        next();
+    } else {
+        if (store.userToken) {
+            next();
+        } else {
+            next({ name: 'login' });
+        }
+    }
+});
 const app = createApp(App);
 const pinia = createPinia();
 pinia.use(piniaPersist);
--- 课堂练习/01-vue/01-vue2基础/03-vue脚手架/02-vite/elementPlusDemo/src/views/login.vue
+++ 课堂练习/01-vue/01-vue2基础/03-vue脚手架/02-vite/elementPlusDemo/src/views/login.vue
@@ -4,7 +4,7 @@ import { reactive, ref } from 'vue';
 import Api from '../api';
 import { userLoginInfo } from '../store/login';
 import { ElLoading, ElMessage } from 'element-plus';
-
+import { useRouter } from 'vue-router';
 // 定义一个名为store的常量，其值为调用userLoginInfo()函数的返回值
 const store = userLoginInfo();
 const changeState = () => {
@@ -51,7 +51,7 @@ const rules = reactive({
 //获取整个form作为DoM对象，需要作为实参传入给发送按钮的点击方法
 const ruleFormRef = ref();
 const loading = ref(false);
-
+const router = useRouter();
 //发送表单信息的方法
 const submitForm = (formEl) => {
     //验证是否由form表单传入，如果没有直接return打断函数的后续执行
@@ -65,11 +65,12 @@ const submitForm = (formEl) => {
                 text: '加载中',
                 background: 'rgba(0, 0, 0, 0.7)'
             });
-            console.log('登录成功');
+            // console.log('登录成功');
 
             let results = await Api.adminInfo.checkLogin(formData);
 
-            console.log(results);
+            // console.log(results);
+
 
             // loading.value = false;
             loading.close();
@@ -82,9 +83,11 @@ const submitForm = (formEl) => {
                     message: results.msg,
                     type: 'success',
                 });
+                // 编程式导航
+                router.push({ name: 'home' });
             } else if (results.status === 'fail') {
                 ElMessage({
-                    message: results.message,
+                    message: '错误',
                     type: 'error',
                 });
             } else {
@@ -101,7 +104,7 @@ const submitForm = (formEl) => {
 };
 </script>
 <template>
-    <PageView class="flex-row j-c a-c">
+    <PageView class="flex-row j-c a-c bg">
         <!-- <button @click="changeState">demo</button> -->
         <div class="login">
             <el-form ref="ruleFormRef" :model="formData" label-width="80px" :rules="rules" status-icon>
@@ -127,5 +130,9 @@ const submitForm = (formEl) => {
 .login {
     width: 380px;
     // display: flex;
+}
+
+.bg {
+    background: linear-gradient(to right, skyblue, blue);
 }
 </style>
