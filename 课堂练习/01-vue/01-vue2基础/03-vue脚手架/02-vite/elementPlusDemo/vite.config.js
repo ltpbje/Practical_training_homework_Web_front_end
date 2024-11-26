e4bdb07d5733270cfaf6b6d3bf6a62faa258a525
ltpbje <19836788252@163.com>
2024-11-26 09:06:55 +0800

    backups

--- 课堂练习/01-vue/01-vue2基础/03-vue脚手架/02-vite/elementPlusDemo/vite.config.js
+++ 课堂练习/01-vue/01-vue2基础/03-vue脚手架/02-vite/elementPlusDemo/vite.config.js
@@ -5,6 +5,19 @@ import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    // 规定触发警告的 chunk 大小。（以 kbs 为单位）
+    chunkSizeWarningLimit: 4096,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  },
   plugins: [
     vue(),
     AutoImport({
