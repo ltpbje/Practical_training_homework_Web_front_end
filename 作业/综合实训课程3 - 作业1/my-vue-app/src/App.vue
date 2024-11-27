cf3d63fbd2fbd416ae65077c4306cb0759ea4a9f
ltpbje <19836788252@163.com>
2024-11-27 18:14:18 +0800

    backups

---
+++ 作业/综合实训课程3 - 作业1/my-vue-app/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import CitySelector from './components/CitySelector.vue';
+</script>
+
+<template>
+  <CitySelector></CitySelector>
+</template>
+
+<style scoped></style>
---
+++ 作业/综合实训课程3 - 作业1/my-vue-app/src/components/CitySelector.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <label for="province">省份：</label>
+    <select id="province" v-model="selectedProvince" @change="handleProvinceChange">
+        <option value="">请选择省份</option>
+        <option v-for="province in provinces" :key="province.value" :value="province.value">{{ province.label }}
+        </option>
+    </select>
+
+    <label for="city">城市：</label>
+    <select id="city" v-model="selectedCity" :disabled="!selectedProvince">
+        <option value="">请选择城市</option>
+        <option v-for="city in cities" :key="city.value" :value="city.value">{{ city.label }}</option>
+    </select>
+
+</template>
+
+<script setup>
+import data from '../assets/data.json';
+console.log(data);
+
+</script>
