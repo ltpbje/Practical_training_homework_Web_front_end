13c5c70d0ede826abc9bd0913fc28cfb9e939403
ltpbje <19836788252@163.com>
2024-11-27 21:12:29 +0800

    backups

--- 作业/综合实训课程3 - 作业1/my-vue-app/src/components/CitySelector.vue
+++ 作业/综合实训课程3 - 作业1/my-vue-app/src/components/CitySelector.vue
@@ -1,22 +1,40 @@
 <template>
 
     <label for="province">省份：</label>
-    <select id="province" v-model="selectedProvince" @change="handleProvinceChange">
+    <select id="province" v-model="selectedProvince" @change="provinceChange">
         <option value="">请选择省份</option>
-        <option v-for="province in provinces" :key="province.value" :value="province.value">{{ province.label }}
+        <option v-for="(item, index) in provinces" :key="item.provinceName" :value="index">
+            {{ item.provinceName }}
         </option>
     </select>
 
     <label for="city">城市：</label>
-    <select id="city" v-model="selectedCity" :disabled="!selectedProvince">
+    <select id="city" :disabled="!selectedProvince">
         <option value="">请选择城市</option>
-        <option v-for="city in cities" :key="city.value" :value="city.value">{{ city.label }}</option>
+        <option v-for="(item, index) in selectedCitys" :key="item.cityName" :value="item.cityName">{{ item.cityName }}
+        </option>
     </select>
 
 </template>
 
 <script setup>
+import { ref } from 'vue';
+// 导入省市数据
 import data from '../assets/data.json';
-console.log(data);
+// console.log(data.provinces);
+// 所有省份数据
+const provinces = ref([]);
+const selectedProvince = ref('');
+provinces.value = data.provinces;
+// console.log(provinces.value);
+
+// 所选省下的所有城市数据
+const selectedCitys = ref([]);
+// 所选省份改变的时候
+const provinceChange = () => {
+    selectedCitys.value = provinces.value[selectedProvince.value].citys;
+    // console.log(selectedCitys.value);
+
+}
 
 </script>
