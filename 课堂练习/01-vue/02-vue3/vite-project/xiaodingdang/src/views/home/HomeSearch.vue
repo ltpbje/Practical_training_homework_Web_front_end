87d31d9a281d2e7e06ba2485ce5f21b36cb11799
ltpbje <19836788252@163.com>
2024-12-02 18:02:02 +0800

    backups

--- 课堂练习/01-vue/02-vue3/vite-project/xiaodingdang/src/views/home/HomeSearch.vue
+++ 课堂练习/01-vue/02-vue3/vite-project/xiaodingdang/src/views/home/HomeSearch.vue
@@ -1,11 +1,53 @@
 <template>
-    <div>
-        HomeSearch
+    <div class="box">
+        <van-search v-model="searchValue" show-action placeholder="请输入搜索关键词">
+            <template #action>
+                <div @click="onClickButton">搜索</div>
+            </template>
+        </van-search>
+        <div class="shop-list">
+            <ShopItem v-for="(item, index) in shopListData" :key="index" :shopInfo="item"></ShopItem>
+        </div>
     </div>
 </template>
 
 <script setup>
+import ShopItem from '@/components/ShopItem.vue';
+import { onMounted, ref } from 'vue';
+import { shopList } from '@/utils/api';
+// 商家列表数据
+let shopListData = ref([]);
+// 搜索值
+let searchValue = ref('');
+let listData = [];
+const onClickButton = () => {
+    // console.log(shopListData.value);
+    const shopList = listData;
+    let search = searchValue.value;
+    const resultList = shopList.filter(item => item.name.search(search) != -1);
+    shopListData.value = resultList;
 
+};
+const getShopList = async () => {
+    listData = (await shopList()).list;
+    shopListData.value = listData;
+};
+onMounted(() => {
+    getShopList();
+});
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.box {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+    .shop-list {
+        // display: flex;
+        flex: 1;
+        overflow: auto;
+    }
+}
+</style>
