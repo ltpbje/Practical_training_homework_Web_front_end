df7d7159380dc8567dbfa56be891e6b2dadc5257
ltpbje <19836788252@163.com>
2024-12-10 21:10:21 +0800

    作业

---
+++ 作业/综合实训课程3-作业3/frontEnd/todoList/backend/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const DButils = require('./utils/DButils');
+const mysql = require('mysql');
+let conn = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: '123456',
+    database: 'data_query_api',
+    multipleStatements: true//开启之后，可以多条sql语句同时执行
+});
+// 测试连接
+conn.connect(err => {
+    console.log(err, '如果为null 就是连接成功');
+});
+// 创建express应用
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use((req, resp, next) => {
+    // 设置响应头，允许跨域访问
+    resp.setHeader("Access-Control-Allow-Origin", "*");
+    resp.setHeader("Access-Control-Allow-Headers", "*");
+    resp.setHeader("Access-Control-Allow-Methods", "*");
+    next();
+});
+
+const DB = new DButils();
+app.get('/getData', async (req, resp) => {
+
+    let sqlStr = `select * from todolist`;
+    let results = await DB.excuteSql(sqlStr);
+    resp.json(results);
+});
+
+
+app.post('/add', async (req, resp) => {
+    // console.log(1);
+
+    let sqlStr = `insert into todolist(title,content,status,startTime,endTime) values (?,?,?,?,?);`;
+    let { title, content, status, startTime, endTime } = req.body;
+    let params = [title, content, status, startTime, endTime];
+    let results = await DB.excuteSql(sqlStr, params);
+    // console.log(results);
+    resp.json(results);
+
+});
+app.get('/delete', async (req, resp) => {
+    let sqlStr = `DELETE FROM todolist WHERE id=?;`;
+    const { id } = req.query;
+    const params = [id];
+    await DB.excuteSql(sqlStr, params);
+    resp.json('ok');
+});
+// app.get('/', (req, resp) => {
+//     console.log(1);
+
+// });
+
+
+
+
+// 创建一个HTTP服务器，并将app作为回调函数传入
+// const server = http.createServer(app);
+app.listen(8080, '0.0.0.0', () => {
+    console.log('server is running.....', 'http://127.0.0.0:8080/');
+});
--- 作业/综合实训课程3-作业3/frontEnd/todoList/my-vue-app/src/components/TodoList.vue
+++ 作业/综合实训课程3-作业3/frontEnd/todoList/my-vue-app/src/components/TodoList.vue
@@ -1,17 +1,87 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
 // import { Calendar, Cell } from 'vant';
-const date = ref('');
-const show = ref(false);
+const formData = ref({
+  endTime: '',
+  startTime: '',
+  status: '待完成',
+  content: '',
+  title: ''
+});
+const showCalendar = ref(false);
 
+// 格式化时间
 const formatDate = (date) => {
-  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 };
-const onConfirm = (value) => {
-  show.value = false;
-  date.value = formatDate(value);
+const onConfirmDate = (value) => {
+  showCalendar.value = false;
+  formData.value.endTime = formatDate(value);
 };
-const dataList = reactive([]);
+const dataList = ref([]);
+function getFormatDate(str) {
+  let date = new Date(str),
+    year = date.getFullYear(), //获取完整的年份(4位)
+    month = date.getMonth() + 1, //获取当前月份(0-11,0代表1月)
+    strDate = date.getDate(); // 获取当前日(1-31)
+  if (month < 10) month = `0${month}`; // 如果月份是个位数，在前面补0
+  if (strDate < 10) strDate = `0${strDate}`; // 如果日是个位数，在前面补0
+
+  return `${year}-${month}-${strDate}`;
+}
+// 添加事件
+async function addEvent() {
+  formData.value.startTime = formatDate(new Date());
+  if (new Date(formData.value.endTime).getTime() - new Date().getTime() <= 0) {
+    formData.value.status = '已超时';
+  }
+  let res = await axios.post('http://localhost:8080/add', formData.value);
+  console.log(res);
+  await getDataList();
+
+  formData.value = {
+    endTime: '',
+    startTime: '',
+    status: '待完成',
+    content: '',
+    title: ''
+  };
+}
+
+// 删除某项
+async function delEvent(id) {
+  showConfirmDialog({
+    message:
+      '确定删除吗?',
+  })
+    .then(async () => {
+      // on confirm
+      await axios.get(`http://localhost:8080/delete?id=${id}`);
+      getDataList();
+    })
+    .catch(() => {
+      // on cancel
+    });
+  // console.log(id);
+
+}
+async function getDataList() {
+  let data = (await axios.get('http://localhost:8080/getData')).data;
+  // console.log(data);
+  dataList.value = data;
+  dataList.value.forEach(item => {
+    if (item.status != '已完成') {
+      if (new Date(item.endTime).getTime() - new Date().getTime() <= 0) {
+        item.status = '已超时';
+      }
+    }
+  });
+
+}
+onMounted(() => {
+  getDataList();
+});
 </script>
 
 <template>
@@ -24,23 +94,25 @@ const dataList = reactive([]);
         </span>
       </div>
       <div class="inp_section">
-        <input type="text" placeholder="请输入您的待办事项标题" class="titleInp ">
-        <input type="text" placeholder="请输入您的待办事项" class="contentInp">
-        <van-cell title="计划完成时间" :value="date" @click="show = true" />
-        <van-calendar v-model:show="show" @confirm="onConfirm" />
-        <van-button type="success" class="addBtn">添加事项</van-button>
+        <input type="text" placeholder="请输入您的待办事项标题" v-model="formData.title" class="titleInp ">
+        <input type="text" placeholder="请输入您的待办事项" v-model="formData.content" class="contentInp">
+        <van-cell title="计划完成时间" :value="formData.endTime" @click="showCalendar = true" />
+        <van-calendar :min-date="new Date('2000-1-1')" v-model:show="showCalendar" @confirm="onConfirmDate" />
+        <van-button type="success" class="addBtn" @click="addEvent">添加事项</van-button>
       </div>
       <div class="list">
         <ul>
-          <li class="list_item">
-            <van-button :type="{ primary: false, noraml: true, }" class="status_tag" size="normal">已完成</van-button>
-            <div class="content_box">
-              <span>标题1</span>
+          <li class="list_item" v-for="(item, index) in dataList" :key="item.id">
+            <van-button
+              :type="{ 'primary': item.status == '已完成', 'warning': item.status == '待完成', 'danger': item.status == '已超时' }"
+              class="status_tag" size="normal">{{ item.status }}</van-button>
+            <div div class="content_box">
+              <span>{{ item.title }}</span>
               <span>
-                创建时间2024-10-10 计划完成时间：2024-10-10
+                创建时间{{ getFormatDate(item.startTime) }} 计划完成时间：{{ getFormatDate(item.endTime) }}
               </span>
             </div>
-            <van-icon name="close" size=".23rem" class="del_btn" />
+            <van-icon name="close" size=".23rem" class="del_btn" @click="delEvent(item.id)" />
           </li>
         </ul>
       </div>
